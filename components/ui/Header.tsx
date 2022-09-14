11a69ce41482c9f1c64f0c4c4ca209121e49e616
Adriel <baranganadriel@gmail.com>
2022-09-14 00:18:04 -0400

    feat: hamburger menu for header when layout is small

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,6 +1,16 @@
 import React from 'react'
 import NextLink from 'next/link'
-import { Box, Link, Stack } from '@chakra-ui/react'
+import {
+  Flex,
+  Link,
+  Stack,
+  IconButton,
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+} from '@chakra-ui/react'
+import { HamburgerIcon } from '@chakra-ui/icons'
 
 interface ItemLinkProps {
   href: string
@@ -19,13 +29,41 @@ const ItemLink: React.FC<ItemLinkProps> = ({ href, children }) => {
 
 export const Header: React.FC = () => {
   return (
-    <Box w='100%' as='nav' bg='black.50' p={6} mb={12}>
-      <Stack direction={{ base: 'column', sm: 'row' }}>
+    <Flex w='100%' as='nav' bg='black.50' p={6} mb={12}>
+      <Stack direction='row' display={{ sm: 'none', md: 'flex' }}>
         <ItemLink href='/'>Home</ItemLink>
         <ItemLink href='/anime/top-animes'>Top Animes</ItemLink>
         <ItemLink href='/manga/top-mangas'>Top Mangas</ItemLink>
         <ItemLink href='/about'>About</ItemLink>
       </Stack>
-    </Box>
+      <Menu isLazy>
+        <MenuButton
+          as={IconButton}
+          bg='black.50'
+          color='white.50'
+          variant='outline'
+          icon={<HamburgerIcon />}
+          aria-label='Options'
+          display={{ sm: 'flex', md: 'none' }}
+          _hover={{
+            background: 'black',
+          }}
+        />
+        <MenuList bg='black.50'>
+          <ItemLink href='/'>
+            <MenuItem>Home</MenuItem>
+          </ItemLink>
+          <ItemLink href='/anime/top-animes'>
+            <MenuItem>Top Animes</MenuItem>
+          </ItemLink>
+          <ItemLink href='/manga/top-mangas'>
+            <MenuItem>Top Mangas</MenuItem>
+          </ItemLink>
+          <ItemLink href='/about'>
+            <MenuItem>About</MenuItem>
+          </ItemLink>
+        </MenuList>
+      </Menu>
+    </Flex>
   )
 }
